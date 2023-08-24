25422e1bb8da13a11c8ed6621da0e733d8bb3db7
K-Takanori <tabo.25.apr.1984@gmail.com>
2023-08-24 15:46:59 +0900

    ファイル構造整理とヘッダーの作成

---
+++ app/features/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <>
+      <a href="./">
+        <p className="bg-red-300 text-black text-3xl text-center">tutChatbot</p>
+      </a>
+    </>
+  );
+}
