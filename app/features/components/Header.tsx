0f8c0ad6b373937dc0f8e7c48e6b24793827ab1c
K-Takanori <tabo.25.apr.1984@gmail.com>
2023-08-25 14:19:33 +0900

    [fix] ヘッダーとフッターを修正

--- app/features/components/Header.tsx
+++ app/features/components/Header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <>
+    <header>
       <a href="./">
-        <p className="bg-red-300 text-black text-3xl text-center">tutChatbot</p>
+        <p className="bg-red-300 text-black text-3xl text-center h-12">tutChatbot</p>
       </a>
-    </>
+    </header>
   );
 }
