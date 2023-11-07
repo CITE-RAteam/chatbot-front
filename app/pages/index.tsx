f7ae54f220dd796dbe48f106618b547c00304083
K-Takanori <tabo.25.apr.1984@gmail.com>
2023-11-07 14:09:12 +0900

    [fix] インポートのパスを修正

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,8 +1,8 @@
-import ChatBox from "@/features/components/ChatBox";
-import Footer from "@/features/components/Footer";
-import Header from "@/features/components/Header";
-import { useChatValue } from "@/features/hooks/useChatValue";
-import { useQuestionError } from "@/features/hooks/useQuestionError";
+import ChatBox from "../features/components/ChatBox";
+import Footer from "../features/components/Footer";
+import Header from "../features/components/Header";
+import { useChatValue } from "../features/hooks/useChatValue";
+import { useQuestionError } from "../features/hooks/useQuestionError";
 
 export default function Home() {
   const { chatValue, isAPIWaiting, setHandleUserChatChange } = useChatValue();
